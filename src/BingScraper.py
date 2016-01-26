f77499a367f1b70c8a104b1c7d30d44c9c90705d
Benjamin Rood <brood@attotech.com>
2016-01-26 16:53:17 -0500

    scraper: execute a set number of queries instead of a randomly selected number
    This shortens the complexity of the script as well as the execution time.

--- src/BingScraper.py
+++ src/BingScraper.py
@@ -13,8 +13,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 DELAY   = (5, 10, 15, 20, 25, 30, 60);
-PCEXECS = (40, 50, 60);
-MBEXECS = (30, 40, 50);
+PCEXECS = 35;
+MBEXECS = 25;
 WORDS   = "http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt";
 
 # Add accounts as tuples of (username, password).
@@ -137,8 +137,6 @@ if __name__ == '__main__':
             
         username = accountPair[0];
         password = accountPair[1];
-        pcexecs  = random.choice( PCEXECS );
-        mbexecs  = random.choice( MBEXECS );
         
         print( "---------------------------------------------------------------" );
         print( "Account: " + username );
@@ -154,35 +152,35 @@ if __name__ == '__main__':
         
         # Retrieve the snapshot of credits before running searches, so that a
         # report on the number of credits earned can be generated.
-         
+           
         # Run PC Searches - 30 of them will net 15 credits for the day.
         # Delay a random amount before each search so that they don't detect 
         # I'm a robot (or at least have a harder time).
-        for i in range( 0, pcexecs ):
+        for i in range( 0, PCEXECS ):
             time.sleep( random.choice( DELAY ) );
             query = random.choice( words );
-            print( "Performing PC search " + str( (i + 1) ) + "/" + str( pcexecs ) + ": " + query );
+            print( "Performing PC search " + str( (i + 1) ) + "/" + str( PCEXECS ) + ": " + query );
             runQuery( driver, query );
-
+  
         print( "" );
-        
+          
         driver.close();
-        
+          
         # Run mobile platform searches - 20 of them will net 10 credits for the
         # day.  As stated above, delay a random amount before each search so 
         # that they don't detect I'm a robot (or at least have a harder time).
         profile = webdriver.FirefoxProfile();
         profile.set_preference( "general.useragent.override", "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0" );
         driver  = webdriver.Firefox( profile );
-        
+          
         login( driver, username, password );
-        
-        for i in range( 0, mbexecs ):
+          
+        for i in range( 0, MBEXECS ):
             time.sleep( random.choice( DELAY ) );
             query = random.choice( words );
-            print( "Performing Mobile search " + str( (i + 1) ) + "/" + str( mbexecs ) + ": " + query );
+            print( "Performing Mobile search " + str( (i + 1) ) + "/" + str( MBEXECS ) + ": " + query );
             runQuery( driver, query );        
-        
+          
         print( "---------------------------------------------------------------" );
         
         # Closing the driver effectively logs out.  A bit inefficient, but it
